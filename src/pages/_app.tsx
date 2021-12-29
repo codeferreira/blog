fb958ae4ecd95659bbb9f9d9dfabe64f9429baa7
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2021-12-29 17:04:00 -0300

    feat: add Chakra UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
-import 'styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+import { css, Global } from '@emotion/react';
+import DefaultLayout from 'layouts/Default';
+
+import type { AppProps } from 'next/app';
+
+import { prismLightTheme } from 'styles/prism';
+import theme from 'styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Global
+        styles={css`
+          ${prismLightTheme}
+        `}
+      />
+      <DefaultLayout>
+        <Component {...pageProps} />
+      </DefaultLayout>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
