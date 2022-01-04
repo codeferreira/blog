f5cdc3840abe69df24a86f7e0130a81da6961aa6
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-01-04 19:10:02 -0300

    feat: add more MDX components

--- src/components/MDXComponents/index.tsx
+++ src/components/MDXComponents/index.tsx
@@ -1,10 +1,24 @@
-import { Code, Heading } from '@chakra-ui/react';
+import { Code, Heading, Link, Text } from '@chakra-ui/react';
 
 const MDXComponents: Record<string, React.ReactNode> = {
-  h1: (props: any) => <Heading as="h1" {...props} />,
-  inlineCode: (props: any) => (
-    <Code colorScheme="purple" fontSize="0.84em" {...props} />
+  h1: (props: unknown) => <Heading as="h1" {...props} />,
+  h2: (props: unknown) => <Heading as="h2" {...props} />,
+  inlineCode: (props: unknown) => (
+    <Code colorScheme="purple" fontSize="0.95em" {...props} />
   ),
+  p: (props: unknown) => (
+    <Text my={5} fontSize="1em" lineHeight="base" {...props} />
+  ),
+  strong: (props: unknown) => (
+    <Text
+      as="strong"
+      fontSize="1em"
+      lineHeight="base"
+      color="#6767C2"
+      {...props}
+    />
+  ),
+  a: (props: unknown) => <Link isExternal color="#6767C2" {...props} />,
 };
 
 export default MDXComponents;
