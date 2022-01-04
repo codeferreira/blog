b840b617a44fe2784c6a3777fb2313a6bee8c1cc
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-01-04 19:10:28 -0300

    feat: add icons and plausible analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,33 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/images/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/images/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
@@ -25,6 +52,11 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,700;1,400&family=Press+Start+2P&display=swap"
             rel="stylesheet"
+          />
+          <script
+            defer
+            data-domain="ferreiracode.com"
+            src="https://plausible.io/js/plausible.js"
           />
         </Head>
         <body>
